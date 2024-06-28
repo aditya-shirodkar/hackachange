daabf007f1bf9dfcd2cfff70115e646f8df65be6
Aditya <adityashirodkar2020@gmail.com>
2024-06-28 22:06:27 +0530

    changed data from tuple to string

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ def main():
 
 
 def load_video():
-    data = (
-        "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0",
-    )
+    data = "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
     vid_placeholder = st.empty()
     vid_placeholder.video(data=data)
 
