2ae8d11980fb2fd1b1cb4c5201fca6c1de2ca398
Aditya <adityashirodkar2020@gmail.com>
2024-06-28 22:10:23 +0530

    moved load video code to main

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def main():
     test = st.sidebar.text_input("Test")
     # llm = load_llm(open_ai_api_key)
 
-    load_video()
+    # load_video()
+
+    data = "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
+    vid_placeholder = st.empty()
+    vid_placeholder.video(data=data)
+
+    button_90 = st.button("Go to 90 secs")
+
+    if button_90:
+        vid_placeholder.empty()
+        vid_placeholder.video(data=data, start_time=90)
 
     st.text_input("Question")
 
