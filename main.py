198d7acf2b7de2e2d4da94ef7e3593c7a2799602
Aditya <adityashirodkar2020@gmail.com>
2024-06-28 22:05:05 +0530

    added hacky button navigation for video

--- main.py
+++ main.py
@@ -11,11 +11,23 @@ def main():
     test = st.sidebar.text_input("Test")
     # llm = load_llm(open_ai_api_key)
 
-    st.video(
-        data="https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
-    )
+    load_video()
 
     st.text_input("Question")
+
+
+def load_video():
+    data = (
+        "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0",
+    )
+    vid_placeholder = st.empty()
+    vid_placeholder.video(data=data)
+
+    button_90 = st.button("Go to 90 secs")
+
+    if button_90:
+        vid_placeholder.empty()
+        vid_placeholder.video(data=data, start_time=90)
 
 
 # def load_llm(open_ai_api_key):
