941413e9cf9dda7cf4650c5931245d3e77d5e007
Aditya <adityashirodkar2020@gmail.com>
2024-06-28 21:09:45 +0530

    first commit main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# pip install streamlit, langchain-openai
+
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Hack-a-change", page_icon="📜")
+    st.title("Hack-a-change")
+
+    open_ai_api_key = st.sidebar.text_input("Input your ChatGPT API key")
+    llm = load_llm(open_ai_api_key)
+
+    st.text_input("Question")
+
+def load_llm(open_ai_api_key):
+    return ChatOpenAI(
+        model="gpt-4o",
+        api_key=open_ai_api_key,
+        temperature=0.5,
+    )
