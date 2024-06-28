ce1e0cd9fc0e99acf7ffeb55409d600dbcc02ea1
Aditya <adityashirodkar2020@gmail.com>
2024-06-28 21:52:49 +0530

    added test video link

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     test = st.sidebar.text_input("Test")
     # llm = load_llm(open_ai_api_key)
 
+    st.video(
+        data="https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
+    )
+
     st.text_input("Question")
 
 
