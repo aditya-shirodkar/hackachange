c30cf9221f1f9286f856a5d0e311ef95e65d6426
Aditya <adityashirodkar2020@gmail.com>
2024-06-29 13:11:06 +0530

    added subtitle files

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # pip install streamlit, langchain-openai
 
-from langchain_openai import ChatOpenAI
 import streamlit as st
 
 
@@ -8,12 +7,32 @@ def main():
     st.set_page_config(page_title="Hack-a-change", page_icon="📜")
     st.title("Hack-a-change")
 
-    test = st.sidebar.text_input("Test")
-    # llm = load_llm(open_ai_api_key)
+    vid_option = st.sidebar.radio(
+        "Which video would you like to process?",
+        (
+            "class 3_contractions-of-words",
+            "Class 9_Social Sciences",
+            "Class 11_Biology_nervous-system",
+        ),
+    )
+    vid = vid_picker(vid_option)
+    load_video(vid)
 
-    # load_video()
+    question = st.text_input("Question")
+    ask_question(question, vid_option)
 
-    data = "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
+
+def vid_picker(vid_option):
+    if vid_option == "class 3_contractions-of-words":
+        return "https://dl.dropboxusercontent.com/scl/fi/gqrwshvzijg6jew9bmypp/class-3_contractions-of-words.mp4?rlkey=f8om8ln3e6tt20n39ei66kugu&st=c03orq2i&dl=0"
+    if vid_option == "Class 9_Social Sciences":
+        return "https://dl.dropboxusercontent.com/scl/fi/h33heodx442mmerepmvog/Class-9_Social-Sciences.mp4?rlkey=x4tni5rnn7948nthgd9a8of2y&st=wxw11s5k&dl=0"
+    if vid_option == "Class 11_Biology_nervous-system":
+        return "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
+
+
+def load_video(vid):
+    data = vid
     vid_placeholder = st.empty()
     vid_placeholder.video(data=data)
 
@@ -23,19 +42,10 @@ def main():
         vid_placeholder.empty()
         vid_placeholder.video(data=data, start_time=90)
 
-    st.text_input("Question")
 
-
-def load_video():
-    data = "https://dl.dropboxusercontent.com/scl/fi/ug8419ue3a8rkm0ey84cz/Class-11_Biology_nervous-system.mp4?rlkey=byk4rx1piopcovwpiyagdc2h3&st=iof5yr53&dl=0"
-    vid_placeholder = st.empty()
-    vid_placeholder.video(data=data)
-
-    button_90 = st.button("Go to 90 secs")
-
-    if button_90:
-        vid_placeholder.empty()
-        vid_placeholder.video(data=data, start_time=90)
+def ask_question(question, vid_option):
+    subtitle_filename = vid_option + ".txt"
+    pass
 
 
 # def load_llm(open_ai_api_key):
